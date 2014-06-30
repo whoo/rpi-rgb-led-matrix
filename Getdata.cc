d9a434ce56a9a87df7aa58b9e262d4fe458809b6
Dominique DERRIER <derrierdo@gmail.com>
2014-06-30 13:11:02 -0400

    Correct thread launch GetData.cc

--- Getdata.cc
+++ Getdata.cc
@@ -1,21 +1,29 @@
 #include "Getdata.h"
-
+#include <stdlib.h>
+#include <sys/wait.h>
 
 void GetData::Run() {
 	char argv[20];
 	char cmd[20];
 
+
 	switch (module)
 	{
-	case 'm': strcpy(cmd,"./scripts/meteo.py"); strcpy(argv,"meteo.py"); break;
-        case 's': strcpy(cmd,"./scripts/song.py"); strcpy(argv,"song.py"); break;
-        case 'f': strcpy(cmd,"./scripts/file.sh"); strcpy(argv,"file.sh"); break;
-	default:         strcpy(cmd,"./scripts/meteo.py"); strcpy(argv,"meteo.py"); break;
+		case 'm': strcpy(cmd,"./scripts/meteo.py"); strcpy(argv,"meteo.py"); break;
+		case 's': strcpy(cmd,"./scripts/song.py"); strcpy(argv,"song.py"); break;
+		case 'f': strcpy(cmd,"./scripts/file.sh"); strcpy(argv,"file.sh"); break;
+		default:         strcpy(cmd,"./scripts/meteo.py"); strcpy(argv,"meteo.py"); break;
 	}
 
-	fprintf(stderr,"GetData %c\n",module);
-	if (!fork())
+
+	pid_t child_pid = fork();
+
+	if (child_pid == 0 )
 	{
-		printf("%d\n",execl(cmd,argv,NULL));
+		execl(cmd,argv,NULL);	
 	}
+
+	int ret;
+	waitpid(-1,&ret,0);
+//	system(cmd);
 }
