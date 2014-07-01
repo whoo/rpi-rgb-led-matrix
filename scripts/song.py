64ef41a26738c3e18ef4cb82ef2d57a75e582657
Dominique DERRIER <derrierdo@gmail.com>
2014-06-30 23:04:08 -0400

    Script modification

--- scripts/song.py
+++ scripts/song.py
@@ -6,33 +6,32 @@ import json,random
 from os import chdir
 
 chdir('/home/Users/dominique/display16x32/dd/scripts');
-accroche=['du fun avec','le plaisir d\'ecouter','du son dans la maison:','dansons avec','chanton avec']
+accroche=['du fun avec','le plaisir d\'ecouter','du son dans la maison:','dansons avec','chanton avec','la vie en musique,','trop cool','ca groove avec']
 url='http://wizz/mpd/run.php?cmd=currentsong'
 
 data=request.urlopen(url)
 val=json.loads(data.read().decode('utf-8'))
 
 
+if hasattr(val,'keys'):
+	if "Artist" in val.keys():
+		artist=val['Artist']
+	else:
+		artist="illustre inconnu";
+	
+	if "Title" in val.keys():
+		titre=val['Title']
+	else:
+		titre='heu ... je sais pas'	
 
-if "Artist" in val.keys():
-	artist=val['Artist']
+	str=accroche[random.randint(0,len(accroche)-1)] + " \"%s\" de %s"%(titre,artist)
+	str=str.replace('é','e')
+	str=str.replace('è','e')
+	str=str.replace('ê','e')
+	str=str.replace('à','a')
 else:
-	artist="illustre inconnu";
+	str="Pas de musique ... c est bien triste tout cela"	
 
-if "Title" in val.keys():
-	titre=val['Title']
-else:
-	titre='heu ... je sais pas'	
-
-
-
-str=accroche[random.randint(0,len(accroche)-1)] + " \"%s\" de %s"%(titre,artist)
-str=str.replace('é','e')
-str=str.replace('è','e')
-str=str.replace('ê','e')
-str=str.replace('à','a')
-
-print (str)
 f=open('../data/file','w',encoding='ISO-8859-1')
 f.write(str)
 f.close()
